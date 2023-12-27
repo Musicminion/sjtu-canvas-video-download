2e81274a211c92bfbecd501c66321804f1c78cef
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-12-28 00:00:51 +0800

    Add aria2/aria2c to .gitignore and update unix-build.sh script

--- unix-build.sh
+++ unix-build.sh
@@ -5,7 +5,7 @@ rm -rf ./dist
 rm -rf ./build
 rm -rf ./SJTU-Canvas视频下载器.spec
 
-pyinstaller -n "SJTU-Canvas视频下载器" -w main.py
+pyinstaller -n "SJTU-Canvas视频下载器" -w main.py 
 mkdir -p dist/dmg
 cp -r dist/SJTU-Canvas视频下载器.app dist/dmg
-create-dmg --volname "SJTU-Canvas视频下载器" "dist/SJTU-Canvas视频下载器.dmg" "dist/dmg/"
+create-dmg --volname "SJTU-Canvas视频下载器" "dist/SJTU-Canvas视频下载器.dmg" "dist/dmg/" 
