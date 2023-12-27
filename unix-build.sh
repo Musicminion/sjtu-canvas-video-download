4932e84a72a40648eed48b15e333a027b74c749f
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-12-27 22:46:35 +0800

    Add Unix build script for SJTU-Canvas视频下载器

---
+++ unix-build.sh
@@ -0,0 +1,11 @@
+# /bin/bash
+
+# 清理旧的打包文件
+rm -rf ./dist
+rm -rf ./build
+rm -rf ./SJTU-Canvas视频下载器.spec
+
+pyinstaller -n "SJTU-Canvas视频下载器" -w main.py
+mkdir -p dist/dmg
+cp -r dist/SJTU-Canvas视频下载器.app dist/dmg
+create-dmg --volname "SJTU-Canvas视频下载器" "dist/SJTU-Canvas视频下载器.dmg" "dist/dmg/"
