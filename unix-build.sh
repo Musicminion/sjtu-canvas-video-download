e1b3305eff7a3a423f481a7debe7c809c2ab200b
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-12-28 10:09:09 +0800

    Update build.yaml and unix-build.sh for creating a macOS application with custom icons and background

--- unix-build.sh
+++ unix-build.sh
@@ -6,7 +6,17 @@ rm -rf ./build
 rm -rf ./SJTU-Canvas视频下载器.spec
 
 # 
-pyinstaller --add-data './aria2/aria2c:./aria2' -n "SJTU-Canvas视频下载器" -w main.py 
+pyinstaller --add-data './aria2/aria2c:./aria2' -n "SJTU-Canvas视频下载器" -w  --icon=icons/canvas.icns main.py 
 mkdir -p dist/dmg
 cp -r dist/SJTU-Canvas视频下载器.app dist/dmg
-create-dmg --volname "SJTU-Canvas视频下载器" "dist/SJTU-Canvas视频下载器.dmg" "dist/dmg/" 
+create-dmg \
+    --volname "SJTU-Canvas视频下载器" \
+    --background "icons/background.png" \
+    --window-pos 200 120 \
+    --window-size 800 400 \
+    --icon-size 100 \
+    --icon SJTU-Canvas视频下载器.app 200 190 \
+    --hide-extension SJTU-Canvas视频下载器.app \
+    --app-drop-link 600 185 \
+    "dist/SJTU-Canvas视频下载器.dmg" \
+    "dist/dmg/" 
