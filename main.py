4ddfcd70bbbb042ad6f34f843e64caaf7552f108
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-12-28 12:13:56 +0800

    Update welcome message in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 MainFrame(root)
 
-print("欢迎使用SJTU Canvas 下载器，如果您是mac、linux用户，请先按照Readme里面的说明安装aria2。")
+print("欢迎使用SJTU Canvas 下载器")
 print("下载课程后，请注意课程的版权哦~")
 
 root.mainloop()
